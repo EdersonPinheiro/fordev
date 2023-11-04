91141160129fc10bf64b72ce27c52b8374aa013e
Ederson Pinheiro <edersonspt@gmail.com>
2023-11-04 12:29:40 -0300

    add Authentication Use Case

---
+++ lib/domain/usecases/authentication.dart
@@ -0,0 +1,11 @@
+import 'package:meta/meta.dart';
+
+import '../entities/account_entity.dart';
+
+abstract class Authentication {
+  Future<AccountEntity> auth({
+    @required email,
+    @required password
+  });
+
+}
